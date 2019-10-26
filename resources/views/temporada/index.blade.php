d45ac9db6312c6c19723e8c09d377364bb062458
Alex Moreno <alex.moreno.costa@gmail.com>
2019-10-26 17:47:14 -0300

    Ajuste no layout da tela de listagem de temporadas

--- resources/views/temporada/index.blade.php
+++ resources/views/temporada/index.blade.php
@@ -9,9 +9,9 @@ Temporadas
 @endsection
 
 @section('conteudo')
-    @foreach ($temporadas as $temporada)
-        <div class="row">
-            <div class='col col-12'>Temporada {{ $temporada->numero }}</div>
-        </div>
-    @endforeach
+    <ul class="list-group">
+        @foreach($temporadas as $temporada)
+            <li class="list-group-item">Temporada {{ $temporada->numero }}</li>
+        @endforeach
+    </ul>
 @endsection
